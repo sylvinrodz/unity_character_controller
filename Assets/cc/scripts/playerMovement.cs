ef8a9b2c81de746d4d773062dadeb42a3606fc39
sylvinrodz <38525116+sylvinrodz@users.noreply.github.com>
2023-06-21 15:27:44 +0530

    Added Charactor Controller

--- Assets/cc/scripts/playerMovement.cs
+++ Assets/cc/scripts/playerMovement.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public float speed;
-    // Start is called before the first frame update
-    void Start()
+    public float rotationSpeed;
+
+    private CharacterController characterController;
+
+    private void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Get value from key/Gamepad
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        // get movement direction in vector and calculate magnitude to transform the charactor
         Vector3 movementDirection = new Vector3(horizontalInput,0, verticalInput);
+        float magnitude = movementDirection.magnitude;
+        magnitude = Mathf.Clamp01((float)magnitude);
         movementDirection.Normalize();
-        transform.Translate(movementDirection * Time.deltaTime * speed); 
+        characterController.SimpleMove(movementDirection * magnitude);
+        //transform.Translate(movementDirection * magnitude * Time.deltaTime * speed, Space.World);
+        
+        // move charactor face to the transform direction
+        if(movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, Time.deltaTime * rotationSpeed);
+        }
     }
 }
